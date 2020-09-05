57633443cd64826bc431ac611806da6f1c5a34e2
potter-x2 <apokensmanx2@gmail.com>
2020-09-05 21:16:23 +0900

    モジュール numSClass の初期チェックイン

---
+++ numSClass/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace numSClass
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ numSClass/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace numSuperClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("何か入力してください:");
+            var input = Console.ReadLine();
+
+            //userが数列か文字列か
+            if (int.TryParse(input, out int inputNum))
+            {
+                NumberClass execNum = new NumberClass();
+                execNum.RefNum(inputNum);
+                execNum.LengthNum(inputNum);
+            }
+            else
+            {
+                StringClass execStr = new StringClass();
+                execStr.LengthStr(input);
+                execStr.DoStr(input);
+
+            }
+        }
+    }
+
+    internal class NumberClass : NumClass
+    {
+        public void RefNum(int num)
+        {
+            OddNum(num);
+        }
+
+        public void LengthNum(int num)
+        {
+            if (num < 10)
+            {
+                Console.WriteLine("この数は１桁です");
+            }
+            else if (num < 100)
+            {
+                Console.WriteLine("この数は２桁です");
+            }
+        }
+    }
+
+    internal class StringClass : StrClass
+    {
+        public void LengthStr(String str)
+        {
+            if (str.Length < 1000)
+            {
+                Console.WriteLine("短文です！");
+            }
+        }
+
+        public void DoStr(String str)
+        {
+            HelloStr(str);
+            ReStr(str);
+        }
+    }
+}
